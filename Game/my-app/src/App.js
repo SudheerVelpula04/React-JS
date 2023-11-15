a787a1ef71c1d455b55532feaa33dd001565e95c
Sudheer Velpula <138425235+SudheerVelpula04@users.noreply.github.com>
2023-11-15 11:56:47 +0530

    Add files via upload

---
+++ Game/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './Components/TicTocGame/TicTacToe';
+
+function App() {
+  return (
+    <div >
+     <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
